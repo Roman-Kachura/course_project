3dc584a38e2083b8202f14783e6214f3680d08da
Roman Kachura <roma.kachyra@gmail.com>
2023-01-21 22:41:32 +0100

    ch1

--- src/components/c2-main/m3-auth/SocialBar.tsx
+++ src/components/c2-main/m3-auth/SocialBar.tsx
@@ -7,8 +7,14 @@ import style from './Auth.module.scss';
 
 export const SocialBar: React.FC = () => {
     const dispatch = useAppDispatch();
-    const authWithGoogle = () => dispatch(authWithGoogleThunk());
-    const authWithFacebook = () => dispatch(authWithFacebookThunk());
+    const authWithGoogle = () => {
+        console.log('google in component')
+        dispatch(authWithGoogleThunk())
+    };
+    const authWithFacebook = () => {
+        console.log('facebook in component')
+        dispatch(authWithFacebookThunk())
+    };
     return (
         <div className={style.social}>
             <Button className={style.socialBtn} variant='outline-danger' onClick={authWithGoogle}><Google/></Button>
